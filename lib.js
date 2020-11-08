06a706c91d79f9d19251d1f9b24ff5e8e2eae948
Timothy <tim87henry@gmail.com>
2020-11-08 23:23:02 +1100

    Added table of books

--- lib.js
+++ lib.js
@@ -5,17 +5,24 @@ function Book(name,author) {
     this.author=author
 }
 
-function addBookToLibrary() {
-    myLibrary[myLibrary.length-1]=this.name
-    console.log("Added book")
+function addBookToLibrary(bk) {
+    myLibrary.push(bk)
 }
 
 function displayBooks() {
-    for (book in myLibrary) {
-        console.log(book.name)
+    for (i=0;i<myLibrary.length;i++) {
+        var list_of_books=document.querySelector("#book_list")
+        var row_count=list_of_books.rows.length;
+        var new_row=list_of_books.insertRow(row_count)
+        var book_cell=new_row.insertCell(0)
+        var author_cell=new_row.insertCell(1)
+        book_cell.innerHTML=myLibrary[i].name
+        author_cell.innerHTML=myLibrary[i].author
     }
 }
 
-b1=new Book("Night without end","Alistair Maclean");
-b1.addBookToLibrary
-displayBooks
+const b1=new Book("Night without end","Alistair Maclean");
+addBookToLibrary(b1);
+const b2=new Book("Ice Station Zebra","Alistair Maclean");
+addBookToLibrary(b2);
+displayBooks();
