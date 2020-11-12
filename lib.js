b556dc8dfca7595448dba9285f54a34085623bb9
Timothy <tim87henry@gmail.com>
2020-11-12 23:56:46 +1100

    Added book page count and has read inputs

--- lib.js
+++ lib.js
@@ -1,8 +1,10 @@
 let myLibrary=[];
 
-function Book(name,author) {
+function Book(name,author,pages,read) {
     this.name=name
     this.author=author
+    this.pages=pages
+    this.read=read
 }
 
 function addBookToLibrary(bk) {
@@ -14,34 +16,57 @@ function displayBooks() {
     list_of_books.innerHTML="";
     for (i=0;i<myLibrary.length;i++) {
         const newDiv = document.createElement("div");
-        newDiv.id="newBook"
+        newDiv.id="newBook";
+
         const bookDiv = document.createElement("div");
-        bookDiv.id="bookDiv"
+        bookDiv.id="bookDiv";
         const bookName = document.createTextNode(myLibrary[i].name);
         bookDiv.appendChild(bookName);
+        
         const authorDiv = document.createElement("div");
-        authorDiv.id="authorDiv"
+        authorDiv.id="authorDiv";
         const authorName = document.createTextNode(myLibrary[i].author)
-        authorDiv.appendChild(authorName)
-        newDiv.appendChild(bookDiv)
-        newDiv.appendChild(authorDiv)
+        authorDiv.appendChild(authorName);
+        
+        const lastRow = document.createElement("div");
+        lastRow.id="lastRow";
+
+        const pagesDiv = document.createElement("div");
+        pagesDiv.id="pagesDiv";
+        const numPages = document.createTextNode(myLibrary[i].pages+"p");
+        pagesDiv.appendChild(numPages);
+
+        const readDiv = document.createElement("div");
+        readDiv.id="readDiv";
+        let hasRead="Not read";
+        if (myLibrary[i].read) {
+            hasRead="Read";
+        }
+        const readBook = document.createTextNode(hasRead);
+        readDiv.appendChild(readBook);
+        
+        newDiv.appendChild(bookDiv);
+        newDiv.appendChild(authorDiv);
+        lastRow.appendChild(pagesDiv);
+        lastRow.appendChild(readDiv);
+        newDiv.appendChild(lastRow);
         list_of_books.appendChild(newDiv);
     }
 }
 
-const b1=new Book("Night without end","Alistair Maclean");
+const b1=new Book("Night without end","Alistair Maclean",350,true);
 addBookToLibrary(b1);
-const b2=new Book("Ice Station Zebra","Alistair Maclean");
+const b2=new Book("Ice Station Zebra","Alistair Maclean",400,true);
 addBookToLibrary(b2);
-const b3=new Book("The Day of Jackal","Frederick Forsyth");
+const b3=new Book("The Day of Jackal","Frederick Forsyth",425,true);
 addBookToLibrary(b3);
-const b4=new Book("The Crimson Flame","Franklin W. Dixson");
+const b4=new Book("The Crimson Flame","Franklin W. Dixson",200,true);
 addBookToLibrary(b4);
-const b5=new Book("My Story","Rafael Nadal");
+const b5=new Book("My Story","Rafael Nadal",150,false);
 addBookToLibrary(b5);
-const b6=new Book("Eye of the Needle","Ken Follett");
+const b6=new Book("Eye of the Needle","Ken Follett",250,true);
 addBookToLibrary(b6);
-const b7=new Book("The Girl with the Dragon Tattoo","Stieg Larsson");
+const b7=new Book("The Girl with the Dragon Tattoo","Stieg Larsson",400,false);
 addBookToLibrary(b7);
 displayBooks();
 
@@ -59,7 +84,8 @@ closeBtn.onclick = function() {
 }
 
 addBtn.onclick = function() {
-    const newBook=new Book(document.getElementById("book_name").value,document.getElementById("author_name").value);
+    //alert(document.getElementById("has_read").value);
+    const newBook=new Book(document.getElementById("book_name").value,document.getElementById("author_name").value,document.getElementById("num_pages").value,document.getElementById("has_read").value=="yes");
     addBookToLibrary(newBook);
     displayBooks();
     modalBox.style.display="none";
